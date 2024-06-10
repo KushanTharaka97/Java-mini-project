bdf6a036ab44a357bef102eb7dbd975117bc20e9
kushan_105754 <kushan.tharaka@axiatadigitallabs.com>
2024-06-10 15:09:18 +0530

    updated the class details from the concrete class

---
+++ src/Animal.java
@@ -0,0 +1,6 @@
+public abstract class Animal {
+    public abstract void makeSound();
+    public void sleep(){
+        System.out.println("This animal is sleeping");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class  Main{
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Cat catObj = new Cat();
+        Dog dogObj = new Dog();
+        catObj.makeSound();
+        dogObj.makeSound();
+
+        catObj.sleep();
     }
 } 
